e93bd9f6bf6caa64c508f5ec8becc058d08df337
PlanetofJay <jae44dv@gmail.com>
2023-03-27 19:08:38 -0400

    update return values

--- Screens/Template.js
+++ Screens/Template.js
@@ -73,7 +73,7 @@ export default function Template({ route, navigation }) {
     }
     else if (e === 'black' && frame === 'single') {
       setWallFrame(require('./Images/singleBlackFrame.png'));
-    }    
+    }
     else if (e === 'wood' && frame === 'single') {
       setWallFrame(require('./Images/singleWoodFrame.png'));
     }
@@ -93,12 +93,98 @@ export default function Template({ route, navigation }) {
       setFrame('double');
       setWallFrame(require('./Images/whiteFrame.png'));
       setWallFrameSelected(require('./Images/selectedDouble.png'));
-      setWallFrameUnselected(require('./Images/unselectedSingle.png')); 
+      setWallFrameUnselected(require('./Images/unselectedSingle.png'));
     }
     else {
-      
+
     }
   }
+
+  return (
+    <View style={styles.container}>
+
+      <StatusBar backgroundColor="#916306" barStyle="light-content" />
+
+      <View style={styles.form}>
+        <View style={[styles.wall, { backgroundColor: wallColor }]}>
+          <View style={styles.screenButton2} underlayColor='#fff'>
+            {picture && <Image source={{ uri: picture }} style={{ width: "100%", height: "100%" }} />}
+            <Image source={(wallFrame)} style={{ marginTop: "-72%", width: "100%", height: "100%" }} />
+          </View>
+        </View>
+      </View>
+
+      <Text style={styles.label}>Change background Color:</Text>
+      <View style={{ flexDirection: "row", marginTop: 10, paddingBottom: 10 }}>
+        <TouchableOpacity style={{
+          marginRight: 10, marginLeft: 10, borderWidth: 1, borderColor: '#707070', height: 20, width: 20,
+          float: 'right', backgroundColor: 'white'
+        }} onPress={() => changeBackground('white')}>
+        </TouchableOpacity>
+        <TouchableOpacity style={{
+          marginRight: 10, marginLeft: 10, borderWidth: 1, borderColor: '#707070', height: 20, width: 20,
+          float: 'right', backgroundColor: '#707070'
+        }} onPress={() => changeBackground('#707070')}>
+        </TouchableOpacity>
+        <TouchableOpacity style={{
+          marginRight: 10, marginLeft: 10, borderWidth: 1, borderColor: '#707070', height: 20, width: 20,
+          float: 'right', backgroundColor: '#805858'
+        }} onPress={() => changeBackground('#805858')}>
+        </TouchableOpacity>
+        <TouchableOpacity style={{
+          marginRight: 10, marginLeft: 10, borderWidth: 1, borderColor: '#707070', height: 20, width: 20,
+          float: 'right', backgroundColor: '#2A7E7E'
+        }} onPress={() => changeBackground('#2A7E7E')}>
+        </TouchableOpacity>
+        <TouchableOpacity style={{
+          marginRight: 10, marginLeft: 10, borderWidth: 1, borderColor: '#707070', height: 20, width: 20,
+          float: 'right', backgroundColor: '#B6B796'
+        }} onPress={() => changeBackground('#B6B796')}>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.label}>Change frame Color:</Text>
+      <View style={{ flexDirection: "row", marginTop: 10, paddingBottom: 10 }}>
+        <TouchableOpacity style={{
+          marginRight: 10, marginLeft: 10, borderWidth: 1, borderColor: '#707070', height: 20, width: 20,
+          float: 'right', backgroundColor: 'white'
+        }} onPress={() => changeFrameBackground('white')}>
+        </TouchableOpacity>
+        <TouchableOpacity style={{
+          marginRight: 10, marginLeft: 10, borderWidth: 1, borderColor: '#707070', height: 20, width: 20,
+          float: 'right', backgroundColor: 'black'
+        }} onPress={() => changeFrameBackground('black')}>
+        </TouchableOpacity>
+        <TouchableOpacity style={{
+          marginRight: 10, marginLeft: 10, borderWidth: 1, borderColor: '#707070', height: 20, width: 20,
+          float: 'right', backgroundColor: '#A15932'
+        }} onPress={() => changeFrameBackground('wood')}>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.label}>Select Template:</Text>
+      <View style={{ flexDirection: "row", marginTop: 10, paddingBottom: 10 }}>
+        <TouchableOpacity style={{ marginLeft: 10, marginTop: 5 }}
+          onPress={() => changeFrame('double')}>
+          <Image source={(wallFrameSelected)} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{ marginLeft: 10, marginTop: 5 }}
+          onPress={() => changeFrame('single')}>
+          <Image source={(wallFrameUnselected)} />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.screenButton3} onPress={() => alert('Template Saving In Next Sprint!')} underlayColor='#fff'>
+        <Text style={styles.buttonText}>Save Template</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{ marginTop: "-120%", marginLeft: "90%" }}
+        onPress={pickImage}>
+        <Image source={require('./Images/pencil.png')} />
+      </TouchableOpacity>
+    </View>
+  );
+
 }
 
 /*
@@ -108,39 +194,60 @@ export default function Template({ route, navigation }) {
  */
 const styles = StyleSheet.create({
   form: {
-    margin: 30,
     marginTop: 60,
     backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
+    width: "100%",
   },
-
   container: {
     backgroundColor: 'white',
     flex: 1,
   },
-
   wall: {
     width: "100%",
     height: 300,
     marginTop: -60,
     alignItems: 'center',
   },
-
   label: {
     fontSize: 18,
     marginLeft: 10,
   },
-
-  logo: {
-    marginTop: 80,
-    width: "100%",
-    height: 160,
-    alignSelf: 'center',
+  textInput: {
+    borderColor: 'black',
+    borderWidth: 2,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+    textAlignVertical: 'top',
+    marginTop: 10,
+    width: "90%"
   },
-  screenButton: {
+  screenButton2: {
+    alignItems: 'center',
+    marginTop: 30,
+    backgroundColor: '#7FBBFF',
+    borderRadius: 0,
+    width: 320,
+    height: 230,
+  },
+  frame: {
+    alignItems: 'center',
+    marginTop: 0,
+    borderRadius: 0,
+    width: 250,
+    height: 230,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  screenButton3: {
     marginRight: 40,
-    marginLeft: 40,
+    marginLeft: 60,
     marginTop: 30,
     paddingTop: 12,
     paddingBottom: 12,
@@ -150,20 +257,4 @@ const styles = StyleSheet.create({
     borderColor: '#fff',
     width: "70%",
   },
-  buttonText: {
-    color: '#fff',
-    textAlign: 'center',
-    paddingLeft: 10,
-    paddingRight: 10
-  },
-  option: {
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    borderWidth: 0.5,
-    borderColor: 'purple',
-    borderRadius: 8,
-    color: 'black',
-    paddingRight: 30 // to ensure the text is never behind the icon
-  }
 });
